fd03ba535e629c56c61b0c60edc6a1afce01d6dc
Sahil kumar Tpss <sahilktpss@gmail.com>
2024-04-21 12:32:23 +0530

    build

---
+++ static/js/275.78b77bc0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhotel=self.webpackChunkhotel||[]).push([[275],{7275:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});t(2791);var h=t(184);const r=function(){return(0,h.jsx)(h.Fragment,{children:(0,h.jsx)("div",{children:(0,h.jsx)("h1",{children:"Page Not Found"})})})}}}]);
+//# sourceMappingURL=275.78b77bc0.chunk.js.map
